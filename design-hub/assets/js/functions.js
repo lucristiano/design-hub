18b3e16312e5b66b547b8f801cde29f5c67bd6d4
otavioluisvidal <otavioluisvidal@gmail.com>
2018-12-12 11:15:22 -0200

    Jquery for nav icon

---
+++ design-hub/assets/js/functions.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    var open = false;
+
+    $('.nav-icon').click(function(){
+        open = !open;
+        
+        if(open) {
+        $(this).attr('src', '{{ site.url }}/../assets/img/nav-close.svg');
+        } else {
+        $(this).attr('src', '{{ site.url }}/../assets/img/nav.svg');
+        }
+    });
+
+});
