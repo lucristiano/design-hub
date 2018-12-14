3bf77bfaa049b5f4fd263d134cf8535969fb72c4
Cristiano <cristiano.silva@quintoandar.com.br>
2018-12-14 18:11:12 -0200

    Directory swap

---
+++ _site/design-hub/assets/js/functions.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    var open = false;
+
+    $('.nav-icon').click(function(){
+        open = !open;
+        
+        if(open) {
+        $(this).attr('src', '/assets/img/nav-close.svg');
+        } else {
+        $(this).attr('src', '/assets/img/nav.svg');
+        }
+    });
+});
